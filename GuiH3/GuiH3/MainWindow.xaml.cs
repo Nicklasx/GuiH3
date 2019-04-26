5ea1694c51e7b8816a2a0dc93623bc774c6e7827
Nicklas <sejenick@live.dk>
2019-04-26 11:47:11 +0200

    no message

---
+++ GuiH3/GuiH3/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GuiH3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GrønTryk_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            grønTryk.Opacity = 1;
+        }
+        private void GrønTryk_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            grønTryk.Opacity = 0;
+        }
+        private void GrønTryk_MouseLeave(object sender, MouseEventArgs e)
+        {
+            grønTryk.Opacity = 0;
+        }
+
+        private void RødTryk_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            rødTryk.Opacity = 1;
+        }
+        private void RødTryk_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            rødTryk.Opacity = 0;
+        }
+        private void RødTryk_MouseLeave(object sender, MouseEventArgs e)
+        {
+            rødTryk.Opacity = 0;
+        }
+        private void GulTryk_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            gulTryk.Opacity = 1;
+        }
+        private void GulTryk_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            gulTryk.Opacity = 0;
+        }
+        private void GulTryk_MouseLeave(object sender, MouseEventArgs e)
+        {
+            gulTryk.Opacity = 0;
+        }
+        private void BlåTryk_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            blåTryk.Opacity = 1;
+        }
+        private void BlåTryk_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            blåTryk.Opacity = 0;
+        }
+        private void BlåTryk_MouseLeave(object sender, MouseEventArgs e)
+        {
+            blåTryk.Opacity = 0;
+        }
+        private void OrangeTryk_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            orangeTryk.Opacity = 1;
+        }
+        private void OrangeTryk_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            orangeTryk.Opacity = 0;
+        }
+        private void OrangeTryk_MouseLeave(object sender, MouseEventArgs e)
+        {
+            orangeTryk.Opacity = 0;
+        }
+    }
+}
